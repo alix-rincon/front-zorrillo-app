4dcf841f383a0989a2f017a5d0095d4dd5d15938
Alix Rincón Carreño <alixshayanna@gmail.com>
2021-12-15 18:46:29 -0500

    se incluye modal perfil del usuario

--- js/profile.js
+++ js/profile.js
@@ -3,11 +3,17 @@ $(document).ready(function() {
     .then(response => response.text())
     .then(data => {
         var d1 = document.body;
-        d1.insertAdjacentHTML('beforeend', data);
+        d1.insertAdjacentHTML('beforeend', data);     
     });
 }); 
 
 function openprofile(){
+    var dataSession = JSON.parse(sessionStorage.getItem("_user"));
+    $("#name").html(dataSession.name);
+    $("#identification").html(dataSession.identification);
+    $("#email").html(dataSession.email);
+    $("#profile").html(dataSession.profile);
+    $("#zone").html(dataSession.zone);
     var myModalEl = document.querySelector('#myModal')
     var modal = bootstrap.Modal.getOrCreateInstance(myModalEl);
     modal.show();
